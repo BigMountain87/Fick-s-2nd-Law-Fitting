8f73e8ef9e2e00f6af4c730c19dce85e806d0046
Sang-Bae Choi <sbchoi129@gmail.com>
2023-01-09 09:21:54 +0900

    파일명, Dmin, Dmax, 두께 값을 직접 입력 하도록 수정

--- main_cui.py
+++ main_cui.py
@@ -1,10 +1,22 @@
 import core
 
-filename = 'test.csv'
-D_min = 1e-14
-D_max = 1e-11
+print("Input filename (ex. test.csv)")
+filename = input()
+
+print("Plese input the range of the expected diffusion coefficient D")
+# minimum Diffusitivity 입력
+print("Input D_min : ? (ex. 1e-14) [m^2/s]")
+D_min = float(input())
+
+######### maximum Diffusitivity 입력
+print("Input D_max : ? (ex. 1e-11) [m^2/s]")
+D_max = float(input())
+
+print("D_step : D/100")
 D_step = 1e-15
-thickness = 22e-6
+######### sample thickness 입력
+print("Input thickness : ? (ex. 22e-6) [m]")
+thickness = float(input())
 
 calc = core.core(filename,D_min,D_max,D_step,thickness)
 calc.excute()
